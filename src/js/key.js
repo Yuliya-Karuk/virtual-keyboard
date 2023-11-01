ebcff6d36abc1a938e79d328891b165a2de2b40c
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-11-01 12:52:53 +0400

    feat: add different CSS for differnt key types

--- src/js/key.js
+++ src/js/key.js
@@ -7,11 +7,17 @@ class Key {
     this.ruValue = data.ru.usual;
     this.ruShifted = data.ru.shifted;
     this.value = this.enValue;
+    this.systemKey = data.system;
   }
 
   renderKey() {
     const li = createElementWithProperties('li', 'keyboard__item');
     const button = createElementWithProperties('button', 'keyboard__button');
+    if (this.systemKey) {
+      for (let i = 0; i < this.systemKey.length; i += 1) {
+        button.classList.add(this.systemKey[i]);
+      }
+    }
     const topSpan = createElementWithProperties('span', 'keyboard__top');
     const bottomSpan = createElementWithProperties('span', 'keyboard__bottom');
     const keyValue = createElementWithProperties('div', 'keyboard__label');
