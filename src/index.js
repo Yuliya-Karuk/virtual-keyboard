820f355628f6de04a3c5e4f79621296844a856cc
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-11-02 13:03:47 +0400

    fix: delete console string

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ import App from './js/app';
 
 const newApp = new App();
 newApp.renderKeys();
-
-console.log('test - check eslint, webpack');
--- src/js/systemKeyHandlers.js
+++ src/js/systemKeyHandlers.js
@@ -1,8 +1,6 @@
 const keysHandlers = {
   Backspace: (textarea, cursor) => {
-    console.log(cursor);
     const newValue = `${textarea.value.slice(0, cursor - 1)}${textarea.value.slice(cursor)}`;
-    console.log(newValue);
     return newValue;
   },
   Tab: (textarea, cursor) => {
